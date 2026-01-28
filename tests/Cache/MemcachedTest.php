263ea5154f80275bb126ef5f49475b5d803e729a
Christer Edvartsen <cogo@starzinger.net>
2026-01-28 21:26:23 +0100

    refactor: use attributes to check for required env vars / settings (#8)

--- tests/Cache/MemcachedTest.php
+++ tests/Cache/MemcachedTest.php
@@ -4,32 +4,28 @@ namespace Imbo\Plugin\MetadataCache\Cache;
 
 use Memcached as PeclMemcached;
 use PHPUnit\Framework\Attributes\CoversClass;
-
-use function extension_loaded;
+use PHPUnit\Framework\Attributes\RequiresEnvironmentVariable;
+use PHPUnit\Framework\Attributes\RequiresPhpExtension;
 
 #[CoversClass(Memcached::class)]
+#[RequiresPhpExtension('memcached')]
+#[RequiresEnvironmentVariable('MEMCACHED_HOST')]
+#[RequiresEnvironmentVariable('MEMCACHED_PORT')]
 class MemcachedTest extends CacheTests
 {
     protected function getAdapter(): Memcached
     {
-        if (!extension_loaded('memcached')) {
-            $this->markTestSkipped('Memcached is not installed');
-        }
-
         $host = (string) getenv('MEMCACHED_HOST');
         $port = (int) getenv('MEMCACHED_PORT');
-
-        if ('' === $host || 0 === $port) {
-            $this->markTestSkipped('Specify both MEMCACHED_HOST and MEMCACHED_PORT in your PHPUnit configuration file to run this test case');
-        }
 
         $memcached = new PeclMemcached();
         $memcached->addServer($host, $port);
 
         /** @var array<mixed>|false */
         $version = $memcached->getVersion();
+
         if (false === $version) {
-            $this->markTestSkipped('Could not connect to Memcached server at '.$host.':'.$port);
+            $this->markTestSkipped('Memcached server is not available.');
         }
 
         return new Memcached($memcached, uniqid('imbo-metadata-cache-test-', true));
